bba5699bf845a7de2266d8b59cdfd60b5174b538
Andrei <andrei.hudalla@outlook.com>
2023-08-15 09:38:44 -0500

    Removed deb and rpm build targets

--- forge.config.js
+++ forge.config.js
@@ -15,13 +15,5 @@ module.exports = {
       name: '@electron-forge/maker-zip',
       platforms: ['darwin'],
     },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
   ],
 };
