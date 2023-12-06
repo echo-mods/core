5cb5b70ac38c14413f224a50951dd01af12e93a6
Andrei Hudalla <andrei.hudalla@outlook.com>
2023-12-06 11:49:10 -0600

    Switched to electron-builder

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '',
-  publicDir: false,
-  plugins: [vue()],
-  build: {
-    minify: false
-  }
+	base: '',
+	publicDir: false,
+	plugins: [vue()],
+	build: {
+		minify: false
+	}
 })
